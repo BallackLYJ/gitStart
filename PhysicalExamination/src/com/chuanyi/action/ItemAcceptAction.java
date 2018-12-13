2efb1c248a42ba614c9f292c5a91dcfac9464859
BallackLYJ <45810999+BallackLYJ@users.noreply.github.com>
2018-12-13 10:10:49 +0800

    添加新工程
    体检散件管理系统1.0

---
+++ PhysicalExamination/src/com/chuanyi/action/ItemAcceptAction.java
@@ -0,0 +1,12 @@
+package com.chuanyi.action;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Scope("prototype")
+@RequestMapping("/back")
+public class ItemAcceptAction {
+
+}
